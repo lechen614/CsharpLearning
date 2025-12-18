b061258391191a5027a0b0776f5cc250357dca16
Le Chen <lechen614@gmail.com>
2025-12-18 17:13:56 -0600

    finished first 4 modules of the part2

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,14 +1,9 @@
-﻿Random dice = new Random();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+﻿string[] orderIds = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
-
-int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue, secondValue);
-
-Console.WriteLine(largerValue);
+foreach (string item in orderIds)
+{
+    if (item.StartsWith("B"))
+    {
+        Console.WriteLine(item);
+    }
+}
